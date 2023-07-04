0224310c05d0dee0a61647dee01701a0d81cf3f4
gilgeunwoo <kesksjxhx@gmail.com>
2023-07-04 16:18:27 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/backend7team/Backend7teamApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend7team;
+
+import io.github.flashvayne.chatgpt.service.ChatgptService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend7teamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Backend7teamApplication.class, args);
+    }
+}
