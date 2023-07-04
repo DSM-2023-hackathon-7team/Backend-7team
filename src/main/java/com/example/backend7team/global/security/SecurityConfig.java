52bed6faf65ce500575ff9ca0c63d9a2bdb6c264
gilgeunwoo <kesksjxhx@gmail.com>
2023-07-04 19:50:08 +0900

    security config

---
+++ src/main/java/com/example/backend7team/global/security/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.example.backend7team.global.security;
+
+import com.example.backend7team.global.security.jwt.JwtTokenProvider;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final ObjectMapper objectMapper;
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.
+                csrf().disable()
+                .cors().and()
+
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/users").permitAll()
+
+                .anyRequest().authenticated()
+                .and()
+                .apply(new FilterConfig(jwtTokenProvider, objectMapper));
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
