adf7e3fd3d9ecec95b3e516e547c650df1fba880
gilgeunwoo <kesksjxhx@gmail.com>
2023-07-05 10:12:40 +0900

    BaseTimeEntity 작동되게 수정

--- src/main/java/com/example/backend7team/global/entity/BaseTimeEntity.java
+++ src/main/java/com/example/backend7team/global/entity/BaseTimeEntity.java
@@ -4,13 +4,16 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public abstract class BaseTimeEntity {
 
